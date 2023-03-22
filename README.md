984f1c42741629c148dcb76dcec19f0ad5496d86
Sambhab <ItIsMeSANS@users.noreply.github.com>
2023-03-23 01:59:38 +0530

    Added main files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Purble-Place-Auto-Tile-Matcher
- Automates purble place
+Windows 7/8.1 support updates has ended. This inspiried me to create a automatic tile matcher or whatever its called.
+
+__**To get started you will need to do:**__
+1/ The get game itself
+(If you are in win10/11 download it from microsoft store from here:)
+[Link to download it](https://www.microsoft.com/en-us/p/purble-place/9pk0nng0mv5n)
+
+2/ Put the PurblePlace.exe.mui into C:\Program Files\Microsoft Games\Purble Place\en-US\
+(I recommend you to create a backup) and replace it
+
+3/ Run the script and follow it`s instructions and enjoy.
+
+**Note: I may update this**
---
+++ tilematchwinner2000.py
@@ -0,0 +1,43 @@
+# Imports time, pyautogui, ctypes and turtle libraries
+import pyautogui
+import time
+import ctypes
+import turtle
+
+# Buttons
+MB_OK = 0x0
+MB_OKCXL = 0x01
+MB_YESNOCXL = 0x03
+MB_YESNO = 0x04
+MB_HELP = 0x4000
+
+# Icons
+ICON_EXCLAIM = 0x30
+ICON_INFO = 0x40
+ICON_STOP = 0x10
+
+# Actual code
+result = int(ctypes.windll.user32.MessageBoxW(0, "Do you want to continue?", "Proceed?", MB_YESNO | ICON_EXCLAIM))
+if result == 6:
+    times = int(turtle.textinput("Enter here", "How many times do you want to repeat?"))
+    time_rep = 0
+    games = 0
+    while  times > time_rep:
+        time.sleep(5)
+        time_rep += 1
+        games += 1
+        pyautogui.FAILSAFE = False
+        pyautogui.moveTo(150, 50)
+        pyautogui.leftClick()
+        pyautogui.moveTo(155, 120)
+        pyautogui.leftClick()
+        if games == 4:
+            time.sleep(10)
+            pyautogui.moveTo(840, 870)
+            pyautogui.leftClick()
+            games -= 4
+            
+            
+
+else:
+    ctypes.windll.user32.MessageBoxW(0, "Abort Initiated", "Aborted", MB_OK | ICON_EXCLAIM)
